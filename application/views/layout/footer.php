d152c2af8cd8a3a338351c00788bd0f630347e52
Omar Hernandez <omher1105@gmail.com>
2018-03-07 00:52:16 -0500

    fix

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,6 +1,6 @@
 <!-- JQuery -->
-<script src="<?= base_url() ?>plantillas/js/jquery-1.12.4.min.js"></script> 
-<script type="text/javascript" href="<?php echo base_url() ?>custom/datatable.js"></script>
+<!-- <script src="<?= base_url() ?>plantillas/js/jquery-1.12.4.min.js"></script>  -->
+<!-- <script type="text/javascript" href="<?php echo base_url() ?>custom/datatable.js"></script> -->
 <!-- Library JS -->
 <!-- <script src="<?= base_url() ?>plantillas/js/lecker_library.js"></script>  -->
 
@@ -12,17 +12,17 @@
 <script src="<?php echo base_url() ?>plantillas/js/bootstrap.min.js"></script>
    
 <!--ALERT-->
-<script src="<?php echo base_url() ?>plantillas/js/sweetalert.min.js"></script>
-<link rel="stylesheet" href="<?php echo base_url() ?>plantillas/css/sweetalert.css">
-<script src="<?php echo base_url() ?>plantillas/js/bootbox.min.js"></script>
+<!-- <script src="<?php echo base_url() ?>plantillas/js/sweetalert.min.js"></script> -->
+<!-- <link rel="stylesheet" href="<?php echo base_url() ?>plantillas/css/sweetalert.css"> -->
+<!-- <script src="<?php echo base_url() ?>plantillas/js/bootbox.min.js"></script> -->
 
 
 <!--CHOSEN-->
-<script src="<?php echo base_url() ?>plantillas/js/chosen/chosen.jquery.min.js"></script>
+<!-- <script src="<?php echo base_url() ?>plantillas/js/chosen/chosen.jquery.min.js"></script> -->
 <link rel="stylesheet" href="<?php echo base_url() ?>plantillas/js/chosen/chosen.css">
 
 <!--CALENDAR-->
-<link type="text/css" rel="stylesheet" src="<?php echo base_url() ?>plantillas/css/bootstrap-datepicker/css/datepicker.css"/>  
+<!-- <link type="text/css" rel="stylesheet" src="<?php echo base_url() ?>plantillas/css/bootstrap-datepicker/css/datepicker.css"/>   -->
 <script src="<?php echo base_url() ?>plantillas/css/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
 <script src="<?php echo base_url() ?>plantillas/css/bootstrap-datepicker/js/locales/bootstrap-datepicker.es.js"></script>  
 <!--TIME-->
--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -5,25 +5,31 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Papeleta - Electrónica</title>
 <!-- Library CSS -->
-<link href= rel="stylesheet" href="<?php echo base_url() ?>plantillas/css/lecker_library.css" >
+<!-- <link href= rel="stylesheet" href="<?php echo base_url() ?>plantillas/css/lecker_library.css" > -->
 <!-- Icons CSS -->
 <link rel="stylesheet" href="<?php echo base_url() ?>plantillas/fonts/themify-icons.css">
-<link rel="stylesheet" href="<?php echo base_url() ?>plantillas/fonts/selima/stylesheet.css">
+<!-- <link rel="stylesheet" href="<?php echo base_url() ?>plantillas/fonts/selima/stylesheet.css"> -->
 <!-- Theme CSS -->
 <link rel="stylesheet" href="<?php echo base_url() ?>plantillas/css/lecker_style.css" >
+  <link rel="stylesheet" href="http://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <!-- Google Fonts -->
 <!-- <link  rel="stylesheet" href="<?= base_url() ?>https:/fonts.googleapis.com/css?family=Bree+Serif|Lato"> -->
 <link href="https://fonts.googleapis.com/css?family=Bree+Serif|Lato" rel="stylesheet">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"> </script>
 
- <script src="<?php echo base_url() ?>plantillas/material/js/materialize.min.js"></script>
+
+ <!-- <script src="<?php echo base_url() ?>plantillas/material/js/materialize.min.js"></script> -->
  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
  <!--<link href="https://fonts.googleapis.com/css?family=Passion+One" rel="stylesheet">-->
  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Passion+One">
 
+
+
   <link rel="stylesheet" href="<?php echo base_url() ?>plantillas/material/css/materialize.min.css">
-  <link rel="stylesheet" href="<?php echo base_url() ?>plantillas/materialize/css/main.css">
-  <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>custom/datatable.css">
+  <!-- <link rel="stylesheet" href="<?php echo base_url() ?>plantillas/materialize/css/main.css"> -->
+  <!-- <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>custom/datatable.css"> -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"> </script>
+  <link href="<?php echo base_url() ?>plantillas/css/bootstrap.min.css" rel="stylesheet">
+  <script src="<?php echo base_url() ?>plantillas/js/bootstrap.min.js"></script>
 
 
 
